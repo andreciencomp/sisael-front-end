48a51ccf30319efb4f93d650d71bec647c1f5813
André Soares <andreciencomp@gmail.com>
2021-06-28 21:19:52 -0300

    Commit Inicial

---
+++ src/componentes/JanelaInicial/JanelaInicial.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './JanelaInicial.css'
+
+function JanelaInicial(){
+
+    return(
+        <div className='container'>
+            <h1> SisAEL</h1>
+
+
+        </div>)
+
+    
+
+}
+
+export default JanelaInicial
