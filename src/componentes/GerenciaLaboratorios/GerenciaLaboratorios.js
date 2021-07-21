68b3b24fcacd11359118f64cca6d9163a4f0f32c
Janetoerick <janetoerick18@gmail.com>
2021-07-21 01:49:09 -0300

    Merge branch 'master' of https://github.com/andreciencomp/sisael-front-end

--- src/componentes/GerenciaLaboratorios/GerenciaLaboratorios.js
+++ src/componentes/GerenciaLaboratorios/GerenciaLaboratorios.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import TelaCadastroLaboratorio from '../TelaCadastroLaboratorio/TelaCadastroLaboratorio'
 import './GerenciaLaboratorios.css'
 
 function GerenciaLaboratorios(){
 
+    const[mostrarModal, setMostrarModal] = useState(false);
 
+    function exibirModal(){
+        if(mostrarModal)
+            setMostrarModal(false);
+        else
+            setMostrarModal(true);
+    }
 
     return (
         <div class="container">
             
             <div class="btn-tab">
-                <button>Cadastrar Laboratorio</button>
+                <button onClick={exibirModal}>Cadastrar Laboratorio</button>
             </div>
             <div class="container-tela">
-                
-                <TelaCadastroLaboratorio/>
+
+                {mostrarModal && <TelaCadastroLaboratorio callback={setMostrarModal}/>}
             </div>
 
         </div>
--- src/componentes/TelaCadastroLaboratorio/TelaCadastroLaboratorio.js
+++ src/componentes/TelaCadastroLaboratorio/TelaCadastroLaboratorio.js
@@ -4,84 +4,90 @@ import './TelaCadastroLaboratorio.css'
 import TelaCadastroSala from '../TelaCadastroSala/TelaCadastroSala'
 import axios from 'axios'
 
-function TelaCadastroLaboratorio(){
+function TelaCadastroLaboratorio() {
 
-    const[listaSala, setListaSala] = useState([])
-    const[sala, setSala] = useState(null);
-    const[idLab, setIdLab] = useState(null);
-    useEffect(()=>{
-        
+    const [listaSala, setListaSala] = useState([])
+    const [sala, setSala] = useState(null);
+
+    useEffect(() => {
+
     });
 
-    function removerSala(e){
-       e.preventDefault();
-       let idx = e.target.value;
-       listaSala.splice(idx,1);
-       setListaSala([...listaSala]);
-       console.log(listaSala);
+    function removerSala(e) {
+        e.preventDefault();
+        let idx = e.target.value;
+        listaSala.splice(idx, 1);
+        setListaSala([...listaSala]);
+        console.log(listaSala);
     }
 
-    function adicionarSala(e){
+    function adicionarSala(e) {
         e.preventDefault();
-        listaSala.push({nome: e.target.nome.value});
-        setSala({nome: e.target.nome.value});
-        e.target.nome.value = "";
-   }
+        if (e.target.nome.value !== "") {
+            listaSala.push({ nome: e.target.nome.value });
+            setSala({ nome: e.target.nome.value });
+            e.target.nome.value = "";
+        }
+    }
 
-   function cadastrarSalas(id){
+    function cadastrarSalas(id) {
+
+        listaSala.map((s, i) =>
+            axios.post('http://localhost:8080/sala/cadastrar/' + id, {
+                nome: s.nome
+            }).then(response => {
+                console.log(response.data);
+            }).catch(error => {
+                console.log(error);
+            })
+        );
+    }
+
+    async function evtCadastrarLaboratorio(e) {
+        e.preventDefault();
+        if (e.target.nomeLab.value !== "") {
+            axios.post('http://localhost:8080/laboratorio/cadastrar', {
+                nome: e.target.nomeLab.value
+            }).then(response => {
+                console.log(response.data);
+                cadastrarSalas(response.data.id);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+
+        e.target.nomeLab.value = "";
        
-    listaSala.map((s,i)=>
-        axios.post('http://localhost:8080/sala/cadastrar/'+id, {
-            nome: s.nome
-        }).then(response =>{
-            console.log(response.data);
-        }).catch(error => {
-            console.log(error);
-        })    
-    );
-   }
-
-   async function evtCadastrarLaboratorio(e){
-    e.preventDefault();
-
-    axios.post('http://localhost:8080/laboratorio/cadastrar', {
-            nome: e.target.nomeLab.value
-        }).then(response =>{
-            console.log(response.data);
-           cadastrarSalas(response.data.id);
-        }).catch(error => {
-            console.log(error);
-        })
     };
 
-    return(
+    return (
         <div className="container-tela">
             <form onSubmit={evtCadastrarLaboratorio}>
-                <input type="text" name="nomeLab" placeholder="Nome do Laboratorio"/>
+                <input type="text" name="nomeLab" placeholder="Nome do Laboratorio" />
                 <div class="form-btn">
                     <button type="submit">Cadastrar</button>
                 </div>
             </form>
-            
+
             <div className="lista-salas">
                 <form className="todo-controle" onSubmit={adicionarSala}>
                     <div>
-                        <input name="nome" type="text" placeholder="Adicionar nome da sala"/>
+                        <input name="nome" type="text" placeholder="Adicionar nome da sala" />
 
                         <button type="submit">Adicionar Sala</button>
                     </div>
                 </form>
                 <div className="lista-salas-todo">
                     <p1>Salas a serem adicionadas</p1>
-                    {listaSala.map((s,i)=>{
-                    return (
-                    <li key={s.nome} className="row-todo">
-                        <TelaCadastroSala sala={s}/>
-                        <button value={i} onClick={removerSala}>Remover</button>
-                    </li> );
-                })}
+                    {listaSala.map((s, i) => {
+                        return (
+                            <li key={s.nome} className="row-todo">
+                                <TelaCadastroSala sala={s} />
+                                <button value={i} onClick={removerSala}>Remover</button>
+                            </li>);
+                    })}
                 </div>
-                
+
 
             </div>
 
